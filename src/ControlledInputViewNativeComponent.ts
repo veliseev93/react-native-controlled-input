63f3e7d85aeaac10d03ac5b80bf255b55de0b026
Василий Елисеев <veliseev@ronasit.com>
2026-03-15 10:37:03 +0600

    feat: export NativeProps interface

--- src/ControlledInputViewNativeComponent.ts
+++ src/ControlledInputViewNativeComponent.ts
@@ -35,7 +35,7 @@ interface InputStyle {
   borderColor?: ColorValue;
 }
 
-interface NativeProps extends ViewProps {
+export interface NativeProps extends ViewProps {
   value?: string;
   inputStyle?: InputStyle;
   onTextChange?: BubblingEventHandler<Readonly<TextChangeEvent>>;
